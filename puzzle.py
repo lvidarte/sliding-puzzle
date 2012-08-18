72040e390b486d2ad94d2b67cee500a4036349ea
Leonardo Vidarte <lvidarte@gmail.com>
2012-08-18 20:05:08 -0300

    Refactoring :P

--- puzzle.py
+++ puzzle.py
@@ -6,150 +6,148 @@ import Tkinter as tk
 from PIL import Image, ImageTk
 
 
+MAX_BOARD_SIZE = 500
+
+
 class Application(tk.Frame):
 
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
+    def __init__(self, image, board_grid=4):
+        tk.Frame.__init__(self)
         self.grid()
-        self.size = 125
-        self.grid = 4
+        self.board_grid = board_grid if board_grid > 2 else 3
+        self.load_image(image)
         self.steps = 0
         self.create_widgets()
         self.create_events()
-        self.load_image()
+        self.create_board()
         self.show()
 
+    def load_image(self, image):
+        image = Image.open(image)
+        board_size = min(image.size)
+        if image.size[0] != image.size[1]:
+            image = image.crop((0, 0, board_size, board_size))
+        if board_size > MAX_BOARD_SIZE:
+            board_size = MAX_BOARD_SIZE
+            image = image.resize((board_size, board_size), Image.ANTIALIAS)
+
+        self.image = image
+        self.board_size = board_size
+        self.piece_size = self.board_size / self.board_grid
+
     def create_widgets(self):
-        width = self.size * self.grid
+        width = self.piece_size * self.board_grid
         height = width
         self.canvas = tk.Canvas(self, width=width, height=height)
         self.canvas.grid()
 
     def create_events(self):
-        self.canvas.bind_all('<KeyPress-Up>', self.move_crop)
-        self.canvas.bind_all('<KeyPress-Down>', self.move_crop)
-        self.canvas.bind_all('<KeyPress-Left>', self.move_crop)
-        self.canvas.bind_all('<KeyPress-Right>', self.move_crop)
+        self.canvas.bind_all('<KeyPress-Up>', self.slide)
+        self.canvas.bind_all('<KeyPress-Down>', self.slide)
+        self.canvas.bind_all('<KeyPress-Left>', self.slide)
+        self.canvas.bind_all('<KeyPress-Right>', self.slide)
         self.canvas.bind_all('<KeyPress-h>', self.help)
 
     def help(self, event):
         if getattr(self, '_img_help_id', None) is None:
-            self._img_help = ImageTk.PhotoImage(self._image)
+            self._img_help = ImageTk.PhotoImage(self.image)
             self._img_help_id = self.canvas.create_image(0, 0,
                     image=self._img_help, anchor=tk.NW)
         else:
             state = self.canvas.itemcget(self._img_help_id, 'state')
-            if state == 'hidden':
-                state = ''
-            else:
-                state = 'hidden'
+            state = 'hidden' if state == '' else ''
             self.canvas.itemconfigure(self._img_help_id, state=state)
 
+    def slide(self, event):
+        pieces = self.get_pieces_around()
+        if event.keysym == 'Up' and pieces['bottom']:
+            self._slide(pieces['bottom'], pieces['center'], (0, -self.piece_size))
+        if event.keysym == 'Down' and pieces['top']:
+            self._slide(pieces['top'], pieces['center'], (0, self.piece_size))
+        if event.keysym == 'Left' and pieces['right']:
+            self._slide(pieces['right'], pieces['center'], (-self.piece_size, 0))
+        if event.keysym == 'Right' and pieces['left']:
+            self._slide(pieces['left'], pieces['center'], (self.piece_size, 0))
 
-    def move_crop(self, event):
-        slides = self.get_slides_around()
+    def _slide(self, from_, to, coord):
+        self.canvas.move(from_['id'], *coord)
+        aux = to['pos_a']
+        to['pos_a'] = from_['pos_a']
+        from_['pos_a'] = aux
+        self.steps += 1
 
-        if event.keysym == 'Up' and slides['bottom']:
-                self.canvas.move(slides['bottom']['id'], 0, -self.size)
-                aux = slides['bottom']['pos_a']
-                slides['bottom']['pos_a'] = slides['center']['pos_a']
-                slides['center']['pos_a'] = aux
-                self.steps += 1
-        if event.keysym == 'Down' and slides['top']:
-                self.canvas.move(slides['top']['id'], 0, self.size)
-                aux = slides['top']['pos_a']
-                slides['top']['pos_a'] = slides['center']['pos_a']
-                slides['center']['pos_a'] = aux
-                self.steps += 1
-        if event.keysym == 'Left' and slides['right']:
-                self.canvas.move(slides['right']['id'], -self.size, 0)
-                aux = slides['right']['pos_a']
-                slides['right']['pos_a'] = slides['center']['pos_a']
-                slides['center']['pos_a'] = aux
-                self.steps += 1
-        if event.keysym == 'Right' and slides['left']:
-                self.canvas.move(slides['left']['id'], self.size, 0)
-                aux = slides['left']['pos_a']
-                slides['left']['pos_a'] = slides['center']['pos_a']
-                slides['center']['pos_a'] = aux
-                self.steps += 1
-
-    def get_slides_around(self):
-        slides = {'center': None,
+    def get_pieces_around(self):
+        pieces = {'center': None,
                   'right' : None,
                   'left'  : None,
                   'top'   : None,
                   'bottom': None}
 
-        for crop in self.image:
-            if not crop['visible']:
-                slides['center'] = crop
+        for piece in self.board:
+            if not piece['visible']:
+                pieces['center'] = piece
                 break
 
-        x0, y0 = slides['center']['pos_a']
+        x0, y0 = pieces['center']['pos_a']
 
-        for crop in self.image:
-            x1, y1 = crop['pos_a']
-            if y0 == y1 and x1 == x0+1:
-                slides['right'] = crop
-            if y0 == y1 and x1 == x0-1:
-                slides['left'] = crop
-            if x0 == x1 and y1 == y0-1:
-                slides['top'] = crop
-            if x0 == x1 and y1 == y0+1:
-                slides['bottom'] = crop
+        for piece in self.board:
+            x1, y1 = piece['pos_a']
+            if y0 == y1 and x1 == x0 + 1:
+                pieces['right'] = piece
+            if y0 == y1 and x1 == x0 - 1:
+                pieces['left'] = piece
+            if x0 == x1 and y1 == y0 - 1:
+                pieces['top'] = piece
+            if x0 == x1 and y1 == y0 + 1:
+                pieces['bottom'] = piece
 
-        return slides
+        return pieces
 
-    def load_image(self):
-        self._image = Image.open("image.png")
-        self.image = []
+    def create_board(self):
+        self.board = []
+        for x in xrange(self.board_grid):
+            for y in xrange(self.board_grid):
+                x0 = x * self.piece_size
+                y0 = y * self.piece_size
+                x1 = x0 + self.piece_size
+                y1 = y0 + self.piece_size
+                image = ImageTk.PhotoImage(self.image.crop((x0, y0, x1, y1)))
+                piece = {'id'     : None,
+                         'image'  : image,
+                         'pos_o'  : (x, y),
+                         'pos_a'  : None,
+                         'visible': True}
+                self.board.append(piece)
 
-        for x in xrange(self.grid):
-            for y in xrange(self.grid):
-                x0 = x * self.size
-                y0 = y * self.size
-                x1 = x0 + self.size
-                y1 = y0 + self.size
-                image = ImageTk.PhotoImage(self._image.crop((x0, y0, x1, y1)))
-                crop = {'id'     : None,
-                        'image'  : image,
-                        'pos_o'  : (x, y),
-                        'pos_a'  : None,
-                        'visible': True}
-                self.image.append(crop)
-
-        self.image[-1]['visible'] = False
+        self.board[-1]['visible'] = False
 
     def show(self):
-        random.shuffle(self.image)
+        random.shuffle(self.board)
 
         index = 0
-        for x in xrange(self.grid):
-            for y in xrange(self.grid):
-                self.image[index]['pos_a'] = (x, y)
-                if self.image[index]['visible']:
-                    x1 = x * self.size
-                    y1 = y * self.size
-                    image = self.image[index]['image']
+        for x in xrange(self.board_grid):
+            for y in xrange(self.board_grid):
+                self.board[index]['pos_a'] = (x, y)
+                if self.board[index]['visible']:
+                    x1 = x * self.piece_size
+                    y1 = y * self.piece_size
+                    image = self.board[index]['image']
                     id = self.canvas.create_image(x1, y1, image=image, anchor=tk.NW)
-                    self.image[index]['id'] = id
+                    self.board[index]['id'] = id
                 index += 1
 
 
 if __name__ == '__main__':
 
-#   from optparse import OptionParser
-#   parser = OptionParser(description="Random maze game")
-#   parser.add_option('-W', '--width', type=int, default=21,
-#                     help="maze width (must be odd)")
-#   parser.add_option('-H', '--height', type=int, default=21,
-#                     help="maze height (must be odd)")
-#   parser.add_option('-s', '--size', type=int, default=10,
-#                     help="cell size")
-#   args, _ = parser.parse_args()
+    from optparse import OptionParser
+    parser = OptionParser(description="Sliding puzzle")
+    parser.add_option('-g', '--board-grid', type=int, default=4,
+                      help="")
+    parser.add_option('-i', '--image', type=str, default='image.png',
+                      help="path to image")
+    args, _ = parser.parse_args()
 
-    app = Application()
+    app = Application(args.image, args.board_grid)
     app.master.title('Sliding puzzle')
     app.mainloop()
 
