cb0cde780ec6dc07be7f7f41ae355fc00028b19a
Leo Vidarte <lvidarte@gmail.com>
2016-10-13 11:26:46 -0300

    Add author and license

--- puzzle.py
+++ puzzle.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Author: Leo Vidarte <http://nerdlabs.com.ar>
+
+This is free software,
+you can redistribute it and/or modify it
+under the terms of the GPL version 3
+as published by the Free Software Foundation.
+
+"""
+
 import random
 import Tkinter as tk
 import tkMessageBox
